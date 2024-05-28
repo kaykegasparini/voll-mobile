0a04e4d2cfef4f5f606e0fefcd1b5d17d185fac2
kaykegasparini <kaykeevandro65@gmail.com>
2024-05-28 17:01:18 -0300

    aparecer o nome no perfil
    arrumado a parte de aparecer o nome do paciente no perfil apos fazer login

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,16 +1,35 @@
 import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base'
 import { Titulo } from '../componentes/Titulo'
+import { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { pegarDadosPaciente } from '../servicos/PacienteServico';
+import { Paciente } from '../interfaces/Paciente';
 
 export default function Perfil(){
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente);
+
+  useEffect(() => {
+    async function dadosPaciente(){
+      const pacienteId = await AsyncStorage.getItem('pacienteId');
+      if (!pacienteId) return null;
+
+      const resultado = await pegarDadosPaciente(pacienteId);
+      if (resultado) {
+        setDadosPaciente(resultado);
+        console.log(resultado);
+      };
+    }
+    dadosPaciente();
+  });
   return(
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
         <Titulo color="blue.500">Meu Perfil</Titulo>
 
-        <Avatar size="xl" source={{ uri: "https://github.com/robertoSRMJunior.png" }} mt={5} />
+        <Avatar size="xl" source={{ uri: "https://github.com/kaykegasparini.png" }} mt={5} />
 
         <Titulo color="blue.500">Informações pessoais</Titulo>
-        <Titulo fontSize="lg" mb={1}>Roberto Moraes</Titulo>
+        <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
         <Text>01/04/1988</Text>
         <Text>São Paulo</Text>
 
