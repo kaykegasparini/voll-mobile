ad0c5ae7e6c97136610b31190c5609a3b5a2b1f2
kaykegasparini <kaykeevandro65@gmail.com>
2024-05-29 14:31:49 -0300

    desafio deslogar
    fazendo uma função para deslogar o usuario

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -4,8 +4,9 @@ import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { pegarDadosPaciente } from '../servicos/PacienteServico';
 import { Paciente } from '../interfaces/Paciente';
+import { Botao } from '../componentes/Botao';
 
-export default function Perfil(){
+export default function Perfil({navigation}){
   const [dadosPaciente, setDadosPaciente] = useState({} as Paciente);
 
   useEffect(() => {
@@ -21,23 +22,36 @@ export default function Perfil(){
     }
     dadosPaciente();
   });
+
+  function Deslogar (){
+    AsyncStorage.removeItem('pacienteId');
+    AsyncStorage.removeItem('token');
+    navigation.replace('Login');
+  }
+
   return(
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
         <Titulo color="blue.500">Meu Perfil</Titulo>
 
-        <Avatar size="xl" source={{ uri: "https://github.com/kaykegasparini.png" }} mt={5} />
+        <Avatar size="xl" source={{ uri: dadosPaciente.imagem }} mt={5} />
 
         <Titulo color="blue.500">Informações pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
-        <Text>01/04/1988</Text>
-        <Text>São Paulo</Text>
+        <Text>dadosPaciente.email</Text>
+        <Text>dadosPaciente.endereco.estado</Text>
 
         <Divider mt={5} />
 
-        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
-        <Text>Bronquite</Text>
-        <Text>Sinusite</Text>
+        <Titulo color="blue.500" mb={1}>Planos de saúde</Titulo>
+        {
+          dadosPaciente.planosSaude?.map((plano, index) => (
+              <Text key={index}>{plano}</Text>
+          ))
+        }
+        <Botao onPress={Deslogar}>
+          Deslogar
+        </Botao>
       </VStack>
     </ScrollView>
   )
